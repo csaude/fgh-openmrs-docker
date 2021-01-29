41fb54bd38a21da533901f3dd24ae3aaf6864b90
jpboane <jpboane@gmail.com>
2021-01-29 10:32:22 +0200

    My changes

--- tomcat/setenv.sh
+++ tomcat/setenv.sh
@@ -1,4 +1,4 @@
-export JAVA_OPTS="-Dfile.encoding=UTF-8 -server -Xms1024m -Xmx3072m -XX:PermSize=512m -XX:MaxPermSize=1536m -Duser.timezone=Africa/Maputo"
+export JAVA_OPTS="-Dfile.encoding=UTF-8 -server -Xms1024m -Xmx3072m -XX:PermSize=512m -XX:MaxPermSize=1536m -Duser.timezone=Africa/Maputo -Djava.library.path=/usr/lib/jvm/java-1.8-openjdk/jre/lib/amd64 -Dgnu.io.rxtx.SerialPorts=/dev/modem:/dev/ttyUSB0:/dev/ttyUSB1:/dev/ttyUSB2:/dev/sendsms"
 export CATALINA_OPTS="-Xdebug -Xrunjdwp:transport=dt_socket,address=8000,server=y,suspend=n"
 export JPDA_OPTS="-agentlib:jdwp=transport=dt_socket,address=8000,server=y,suspend=n"
 export OPENMRS_RUNTIME_PROPERTIES_FILE='/usr/local/tomcat/.OpenMRS/openmrs-runtime.properties'
